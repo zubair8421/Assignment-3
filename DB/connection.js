a9f2226687a0ba1f73f91481a35049d137f04713
zubair8421 <75317575+zubair8421@users.noreply.github.com>
2020-12-03 20:27:28 +0500

    1st

--- DB/connection.js
+++ DB/connection.js
@@ -1,6 +1,6 @@
 const mongoose= require('mongoose');
 
-const URI= 'mongodb+srv://ammar123:9000@shopping.y2z1m.mongodb.net/shoppingproducts?retryWrites=true&w=majority';
+const URI= 'mongodb+srv://zubair:1234@cluster0.t5cvz.mongodb.net/assignment?retryWrites=true&w=majority';
 
 const connectDB=async()=>{
     await mongoose.connect(URI,{useNewUrlParser:true, useUnifiedTopology: true });
